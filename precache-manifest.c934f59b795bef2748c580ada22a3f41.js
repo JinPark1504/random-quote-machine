b1cdd132a5bd5d348185153f4c583ca9f8736eba
Jin <jinpark1504@gmail.com>
2019-06-29 17:10:38 -0400

    Updates

---
+++ precache-manifest.c934f59b795bef2748c580ada22a3f41.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09555d9f7cf99ee59a4d5195eaa031d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b23dd41e0b027aa81562",
+    "url": "/static/css/main.f339cf38.chunk.css"
+  },
+  {
+    "revision": "3eb7521ab2d38916805a",
+    "url": "/static/js/2.fb36fda2.chunk.js"
+  },
+  {
+    "revision": "b23dd41e0b027aa81562",
+    "url": "/static/js/main.a16480bc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
