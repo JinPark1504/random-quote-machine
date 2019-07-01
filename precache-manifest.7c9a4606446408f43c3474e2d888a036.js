c722542e2d2f254962678b9a82c8f91e183ee77b
Jin <jinpark1504@gmail.com>
2019-07-01 16:58:48 -0400

    Updates

--- precache-manifest.7c9a4606446408f43c3474e2d888a036.js
+++ precache-manifest.7c9a4606446408f43c3474e2d888a036.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4e3b8e38d5a2e3849358279d4f9d15e4",
+    "revision": "ea3942ea7615ddcb368b0d289b18a6af",
     "url": "/random-quote-machine/index.html"
   },
   {
-    "revision": "b6f4dad0266ffd04ee5e",
-    "url": "/random-quote-machine/static/css/main.2ae559a9.chunk.css"
+    "revision": "0d5ae01283610ea54395",
+    "url": "/random-quote-machine/static/css/main.371c40a5.chunk.css"
   },
   {
     "revision": "3eb7521ab2d38916805a",
     "url": "/random-quote-machine/static/js/2.fb36fda2.chunk.js"
   },
   {
-    "revision": "b6f4dad0266ffd04ee5e",
+    "revision": "0d5ae01283610ea54395",
     "url": "/random-quote-machine/static/js/main.f6a9e632.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/random-quote-machine/precache-manifest.66944148b04bdbdb5cb68f476b21d583.js"
+  "/random-quote-machine/precache-manifest.7c9a4606446408f43c3474e2d888a036.js"
 );
 
 self.addEventListener('message', (event) => {
